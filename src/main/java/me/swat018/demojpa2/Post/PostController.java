269a5afe60faba70950337e02d3383d4c592fc9b
jinwoo park <swat018@gmail.com>
2022-02-19 15:29:22 +0900

    feat : 스프링 데이터 Common - 웹 기능 소개

---
+++ src/main/java/me/swat018/demojpa2/Post/PostController.java
@@ -0,0 +1,27 @@
+package me.swat018.demojpa2.Post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @GetMapping("/posts/{id}")
+    public String getAPost(@PathVariable("id") Post post) {
+        return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public PagedModel<EntityModel<Post>> getPosts(Pageable pageable, PagedResourcesAssembler<Post> assembler) {
+        return assembler.toModel(postRepository.findAll(pageable));
+    }
+}
